489eee5ed97a514de35d4467794f2bd87936c426
Julia <usey85@mail.ru>
2018-03-22 22:01:44 +0300

    Домашнее задание №1

--- homework/homework1/src/homework1.java
+++ homework/homework1/src/homework1.java
@@ -2,11 +2,11 @@ public class homework1 {
     public static void main (String [] args){
         Part1 p1 = new Part1();
         Part2 p2 = new Part2();
-//        System.out.println("Часть 1:");
-//        p1.closer_1();
-//        p1.equation_2();
-//        p1.maxNumeral_3();
-//        p1.sortIfFor_4();
+        System.out.println("Часть 1:");
+        p1.closer_1();
+        p1.equation_2();
+        p1.maxNumeral_3();
+        p1.sortIfFor_4();
         System.out.println("\nЧасть 2:");
         p2.numbers_1();
         p2.numbers_2();

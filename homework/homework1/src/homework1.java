32071c2fb8a84ec9a7bd76c848b6071ab227a87a
Julia <usey85@mail.ru>
2018-03-22 22:00:29 +0300

    Домашнее задание №1

--- homework/homework1/src/homework1.java
+++ homework/homework1/src/homework1.java
@@ -1,2 +1,19 @@
 public class homework1 {
+    public static void main (String [] args){
+        Part1 p1 = new Part1();
+        Part2 p2 = new Part2();
+//        System.out.println("Часть 1:");
+//        p1.closer_1();
+//        p1.equation_2();
+//        p1.maxNumeral_3();
+//        p1.sortIfFor_4();
+        System.out.println("\nЧасть 2:");
+        p2.numbers_1();
+        p2.numbers_2();
+        p2.numbers_3();
+        p2.prime_4();
+        p2.fibonacci_5();
+        p2.lukyTicket_6();
+        p2.watch_7();
+    }
 }

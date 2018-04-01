a0e336f68a6b12c7d96971615bee475be790c7fe
Julia <usey85@mail.ru>
2018-04-01 23:32:54 +0300

    Калькулятор

--- Lessons/Lessons4/src/calc/Div.java
+++ Lessons/Lessons4/src/calc/Div.java
@@ -4,6 +4,10 @@ public class Div extends Operation {
 //деление
     @Override
     public int execute(int a, int b){
-        return a/b;
+        if (b > 0) {
+            return a / b;
+        }
+        System.out.println("На ноль делить нельзя");
+        return 0;
     }
 }

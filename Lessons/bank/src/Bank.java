111e6e781335a1bea28ae41db281776b056ae8c8
Julia <usey85@mail.ru>
2018-06-04 18:59:55 +0300

    bank_part1

--- Lessons/bank/src/Bank.java
+++ Lessons/bank/src/Bank.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+
 public class Bank {
 
-    private static BlockingQueue<Account> listAcc = new LinkedBlockingQueue<>();
+    private static BlockingQueue<Account> queueAcc = new LinkedBlockingQueue<>();
+    private static BlockingQueue<Transaction> notifier = new LinkedBlockingQueue<>();
+    private static List<Thread> threadList = new ArrayList<>();
+
+    private static List<User> userList = new ArrayList<>();
+    private static List<Account> listAcc = new ArrayList<>();
+    private static List<Transaction> transactions = new ArrayList<>();
 
     public static boolean transferMoney(Account src, Account dst, int amount){
 //        System.out.println(Thread.currentThread().getName());
@@ -21,25 +31,58 @@ public class Bank {
         }
     }
 
-    public static void main(String[] args) {
-        listAcc.add(new Account(new User("Are@net"),15000));
-        listAcc.add(new Account(new User("Bre@net"),9000));
-        listAcc.add(new Account(new User("Cre@net"),18000));
-        listAcc.add(new Account(new User("Dre@net"),21000));
+    private static void sendMesseng(Transaction t){
+        for (Account acc: listAcc){
+            if (t.getDstAccId()==acc.getId()) {
+                System.out.println(t.sendMess()+ " - " + getUserEmail(acc.getUserId()));
+            }
+        }
+    }
 
-        while(!listAcc.isEmpty()) {
+    private static String getUserEmail(int userId){
+        for (User user: userList){
+            if(user.getId()==userId) return user.getEmail();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Random ran = new Random();
+        for(int i = 0; i < 4; i++){
+            User user = new User("Are@net"+i);
+            userList.add(user);
+            listAcc.add(new Account(user, (int) (10000*(ran.nextInt(10)+1))));
+        }
+        queueAcc.addAll(listAcc);
+
+        while(!queueAcc.isEmpty()) {
             Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     try {
-//                        System.out.println(transferMoney(listAcc.take(),listAcc.take(),17000));
-                        transferMoney(listAcc.take(),listAcc.take(),5000);
+                        Account src = queueAcc.take();
+                        Account dst = queueAcc.take();
+                        System.out.println(src.getAmount() + " - " + dst.getAmount());
+                        int amount = 5000;
+                        if(transferMoney(src,dst,amount)){
+                            System.out.println(Thread.currentThread().getName());
+                            notifier.add(new Transaction(src.getId(),dst.getId(),amount));
+                        }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
             });
+            threadList.add(thread);
+            thread.setDaemon(true);
             thread.start();
+        }
+        System.out.println("========");
+        for (Thread thread: threadList){
+            thread.join();
+        }
+        while (!notifier.isEmpty()) {
+            sendMesseng(notifier.take());
         }
     }
 
--- Lessons/bank/src/Transaction.java
+++ Lessons/bank/src/Transaction.java
@@ -1,2 +1,34 @@
+
 public class Transaction {
+    private static long countId = 1000l;
+    private long id;
+    private int srcAccId;
+    private int dstAccId;
+    private int anount;
+
+    public Transaction(int srcAccId, int dstAccId, int anount) {
+        this.id = countId++;
+        this.srcAccId = srcAccId;
+        this.dstAccId = dstAccId;
+        this.anount = anount;
+    }
+
+    public int getSrcAccId() {
+        return srcAccId;
+    }
+
+    public int getDstAccId() {
+        return dstAccId;
+    }
+
+    public int getAnount() {
+        return anount;
+    }
+
+    public String sendMess(){
+        return ("Перевод выполнен успешно!");
+    }
+
+    // каждый успешный перевод создаёт Transaction и добавляет его в очередь для потока Notifier
+// Notifier берет транзакцию и отправляет пистма её учасникам (пишет в консоль)
 }
